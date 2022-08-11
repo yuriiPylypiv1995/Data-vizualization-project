9e50391ad7a2366045a27b1095409fe3e853220c
Yurii Pylypiv <yra.pilipsv@gmail.com>
2022-08-11 22:42:16 +0300

    RandowWalk refactoring. new method get_step added

--- random_walk.py
+++ random_walk.py
@@ -15,16 +15,11 @@ class RandowWalk:
 
         while len(self.x_values) < self.num_points:
 
-            # Choice direction and duration
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4])
-            x_step = x_direction * x_distance
+            # Set point's steps on a diagram
+            x_step = self.get_step()
+            y_step = self.get_step()
 
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4])
-            y_step = y_direction * y_distance
-
-            # Skip steps  that do nothing
+            # Skip steps that do nothing
             if x_step == 0 and y_step == 0:
                 continue
 
@@ -34,3 +29,14 @@ class RandowWalk:
 
             self.x_values.append(x)
             self.y_values.append(y)
+
+    @staticmethod
+    def get_step():
+        """This method calculate steps for 'x' and 'y' axis of diagrams"""
+
+        # Choice direction and duration
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        step = direction * distance
+
+        return step
